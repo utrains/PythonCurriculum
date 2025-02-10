58792fdcc55f5840447d64fdd26857b9cec526cd
infofitsoftware <infofitsoftware@gmail.com>
2025-02-10 18:12:36 +0530

    commit

---
+++ Module2/exercise.py
@@ -0,0 +1,9 @@
+"""
+1. Declare Variables for Your Name, Age, and Height
+Task: Create variables for your name (string), age (integer), and height (float), and print them.
+"""
+
+"""
+2. Convert an Integer to a String and Print Its Type
+Task: Convert an integer 100 into a string and print its type.
+"""
