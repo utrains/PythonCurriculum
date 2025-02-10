0d6c24d9532b4700766a06e59b3bef97ae1188e3
infofitsoftware <infofitsoftware@gmail.com>
2025-02-10 19:03:02 +0530

    commit

---
+++ Module3/exercise.py
@@ -0,0 +1,10 @@
+"""
+1. Swap Two Numbers Using Arithmetic Operations
+Task: Swap two numbers without using a third variable.
+
+"""
+
+"""
+2. Check If a Number Is Even or Odd
+Task: Write a program to check whether a number is even or odd using the modulus operator.
+"""
---
+++ Module3/interview.py
@@ -0,0 +1,14 @@
+"""
+1. What is the Difference Between == and is?
+Answer:
+
+== checks if values are the same.
+is checks if objects are the same (memory location).
+
+"""
+
+"""
+2. Explain Short-Circuit Evaluation in Python.
+Answer:
+Short-circuit evaluation means Python stops evaluating a logical expression as soon as the result is known.
+"""
