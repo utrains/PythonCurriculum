18cbd083a6aea1cfb830b0f15b35c7e058b2ca92
infofitsoftware <infofitsoftware@gmail.com>
2025-02-13 18:25:08 +0530

    commit

---
+++ Module6/exercise.py
@@ -0,0 +1,25 @@
+"""Create a tuple with 5 elements and print the third and last element.
+Convert a tuple into a list, modify the list, and convert it back to a tuple.
+Find and count how many times a specific element appears in a tuple.
+Write a Python program to swap two values using tuple unpacking.
+Find the sum and average of all numbers in a tuple.
+
+
+
+Create a dictionary storing student details and print the student’s name and grade.
+Write a Python program to check if a key exists in a dictionary.
+Merge two dictionaries and print the updated dictionary.
+Iterate over a dictionary and print all key-value pairs.
+Create a dictionary with numbers as keys and their squares as values using comprehension.
+
+Create a set of your favorite movies and print them.
+Add a new movie to the set and remove an old one.
+Find the union and intersection of two sets of numbers.
+Write a Python program to check if a number exists in a set.
+Create a set of square numbers using set comprehension.
+
+"""
+
+
+
+
